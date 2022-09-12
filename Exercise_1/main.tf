e08b998e85c973676d8c6a0c972447d0cfe14a6d
Christopher Sherman <chris@shermandigital.com>
2022-09-12 09:26:52 -0400

    Add AZ names, subnet CIDRS, and Elastic IP to diagram 1. Add NAT gateway, load balancers, and Elastic IP to cost estimations. Tag Exercise 1 Terraform instances with names.

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,7 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  access_key = "AKIAZNPRZ5M7UUJRQS4K"
-  secret_key = "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"
+  access_key = "AKIA6MJMDQA7PUWZYVJR"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
@@ -10,6 +10,10 @@ resource "aws_instance" "Udacity_T2" {
   count         = "4"
   ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "Udacity_T2"
+  }
 }
 
 
@@ -19,5 +23,9 @@ resource "aws_instance" "Udacity_M4" {
   count         = "0"
   ami           = "ami-0323c3dd2da7fb37d"
   instance_type = "m4.large"
+
+  tags = {
+    Name = "Udacity_M4"
+  }
 }
 
